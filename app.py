36c718e1aadc59a65b9941670eec3b26f735dce5
Anastasia <stanleya@udel.edu>
2026-03-30 12:26:40 -0400

    app set up

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import json
+from pathlib import Path
+from datetime import datetime
+import uuid
+import time
+
+
+st.set_page_config(page_title="Event Manager", layout="centered")
+st.title("Login")
+
