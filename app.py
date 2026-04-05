f2e69a548620015634339c634b91166bfe4edcd9
Matthew <mmusacch@udel.edu>
2026-04-05 13:43:54 -0400

    final push?

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import time
 
 
 st.set_page_config(page_title="Event Manager", layout="centered")
-st.title("Login")
+
 
 
 
@@ -22,18 +22,36 @@ if "role" not in st.session_state:
 if "page" not in st.session_state:
     st.session_state["page"] = "login"
 
-users = [
+
+
+
+#json_path = Path("event.json")
+#if json_path.exists(): 
+#   with open(json_path, "r") as f:
+#        event = json.load(f)
+
+
+users_file = Path("users.json")
+if users_file.exists():
+   with open(users_file, "r") as f:
+        users = json.load(f)
+else:
+    users = [
      {
     "id": "1",
     "email": "admin@event.edu",
     "full_name": "System Admin",
     "password": "123ssag@43AE",
-    "role": "Admin",
-    "registered_at": "..."
+    "role": "Admin"
     }
 ] 
 
-events = [
+events_file = Path("events.json")
+if events_file.exists():
+   with open(events_file, "r") as f:
+        events = json.load(f)
+else:
+    events = [
   {
     "id": "1",
     "name": "Graduation",
@@ -56,36 +74,22 @@ events = [
   }
 ]
 
-json_path = Path("event.json")
-if json_path.exists(): 
-    with open(json_path, "r") as f:
-        event = json.load(f)
 
+if not st.session_state["logged_in"]:
+    st.title("Log in")
 
-users_file = Path("users.json")
-events_file = Path("events.json")
-
-users = json.load(open(users.file)) 
-if users_file.exists():
-   with open(users_file, "r") as f:
-        user = json.load(f)
-
-events = json.load(open(events.file)) 
-if events_file.exists():
-   with open(events.file, "r") as f:
-     event = json.load(f)
     tab1, tab2 = st.tabs(["Login", "Registration"])
     with tab1:
         st.subheader("Login")
-        email = st.text_input("Email", key="login_email")
-        password = st.text_input("Password", type="password", key="login_password")
+        email_input = st.text_input("Email", key="login_email")
+        pass_input = st.text_input("Password", type="password", key="login_password")
             
         if st.button("Login"):
             with st.spinner("Logging in..."):
                 time.sleep(2)
                 found_user = None
                 for user in users:
-                    if user["email"].strip().lower() == email.strip().lower() and user["password"] == password:
+                    if user["email"].strip().lower() == email_input.strip().lower() and user["password"] == pass_input:
                         found_user = user
                         break
                     
@@ -102,20 +106,74 @@ if events_file.exists():
 
     with tab2:
         st.subheader("Register")
-        new_email = st.text_input("New Email")
-        new_pass = st.text_input("New Password", type="password")
+        email_input = st.text_input("Email", key="reg_email")
+        name_input = st.text_input("Full Name", key="reg_name")
+        pass_input = st.text_input("Password", type="password", key="reg_password")
+        role_input = st.selectbox("Role", ["Attendee", "Admin"], key="reg_role")
 
         if st.button("Create Account"):
             users.append({
                 "id": str(uuid.uuid4()),
-                "email": new_email,
-                "password": new_pass,
-                "role": "Attendee"
+                "email": email_input,
+                "full_name": name_input,
+                "password": pass_input,
+                "role": role_input
             })
-            with open(json_path, "w") as f:
+            with open(users_file, "w") as f:
                 json.dump(users, f, indent=4)
+                with open(users_file, "r") as f:
+                    users = json.load(f)
             st.success("Account created!")
+
+
+
+
+
+
+if st.session_state["role"] == "Attendee":
+    if st.session_state["page"] == "home":
+        st.markdown("welcome! View Upcoming Events!")
+        if st.button("Event Portal", key="view_events_btn", type="primary", use_container_width=True):
+            st.session_state["page"] = "attendee_portal"
             st.rerun()
+ 
+    if st.session_state["page"] == "attendee_portal":
+
+        st.title("Find Events")
+
+        event_names = []
+        for event in events:
+            event_names.append(event["name"])
+
+        selected_event_name = st.selectbox("Select Event", event_names)
+
+        selected_event = None
+        for event in events:
+            if event["name"] == selected_event_name:
+                selected_event = event
+
+        if selected_event:
+            st.markdown(f"Event: {selected_event['name']}")
+            st.write(f" Date/Time: {selected_event['date']} at {selected_event['time']}")
+            st.write(f"Location:{selected_event['location']}")
+            st.write(f"{selected_event['description']}")
+            st.write(f"Tickets Available: {selected_event['tickets'] - selected_event['reserved']}")
+
+            if st.button("Reserve Ticket"):
+                if selected_event["reserved"] < selected_event["tickets"]:
+                    selected_event["reserved"] += 1
+                    with open(events_file, "w") as f:
+                        json.dump(events, f, indent=4)
+                    with open(events_file, "r") as f:
+                        events = json.load(f)
+                    st.rerun()
+                    st.success("Ticket reserved!")
+                else:
+                    st.error("Sold out")
+
+                
+
+
 
 
 if st.session_state["role"] == "Admin":
@@ -125,114 +183,78 @@ if st.session_state["role"] == "Admin":
             st.session_state["page"] = "dashboard"
             st.rerun()
 
-    tab1, tab2, tab3 = st.tabs(["Create Event", "View Event", "Update Event"])
+        if events_file.exists():
+            with open(events_file, "r") as f:
+                events = json.load(f)
+        else:
+            events = []
 
+    tab1, tab2, tab3 = st.tabs(["Create Event", "View Events", "Update Event"])
+    with tab1:
+        st.subheader("Create New Event")
+        name_input = st.text_input("Event Name", key="create_name")
+        date_input = st.text_input("Date", key="create_date")
+        time_input= st.text_input("Time", key="create_time")
+        location_input = st.text_input("Location", key="create_location")
+        description_input = st.text_area("Description", key="create_description")
+        tickets_input = st.number_input("Tickets", min_value=1, key="create_ticket")
 
-#login 
-else:
-    st.title("Event Manager App")
-
-    st.subheader("Log In")
-    with st.container(border=True):
-        email_input = st.text_input("Email", key="login_email")
-        password_input = st.text_input("Password", type="password", key="login_password")
-        
-        if st.button("Log In", type="secondary", use_container_width=True):
-            with st.spinner("Logging in..."):
-                time.sleep(2) 
-                # Find user
-                found_user = None
-                for user in users:
-                    if user["email"].strip().lower() == email_input.strip().lower() and user["password"] == password_input:
-                        found_user = user
-                        break
-                
-                if found_user:
-                    st.success(f"Welcome back, {found_user['email']}!")
-                    st.session_state["logged_in"] = True
-                    st.session_state["user"] = found_user
-                    st.session_state["role"] = found_user["role"]
-                    st.session_state["page"] = "home"
-
-                    time.sleep(2)
-                    st.rerun()
-                else:
-                    st.error("Invalid credentials")
-        
-
-
-
-    # --- REGISTRATION ---
-    st.subheader("New Admin Account")
-    with st.container(border=True):
-        new_email = st.text_input("Email Address", key="reg_email")
-        new_password = st.text_input("Password", type="password", key="reg_password")
-        
-        if st.button("Create Account", type="secondary", use_container_width=True):
-            with st.spinner("Creating account..."):
-                time.sleep(2) # Fake backend delay
-                # ... (Assume validation logic here) ...
-                users.append({
-                    "id": str(uuid.uuid4()),
-                    "email": new_email,
-                    "password": new_password,
-                    "role": "Admin"
-                })
-                with open(json_path, "w") as f:
-                    json.dump(users, f, indent=4)
-                st.success("Account created!")
-                st.rerun()
-
-    st.write("---")
-#REMOVE THIS BEFORE SUBMISSION
-    st.dataframe(users)
-
-    #i dont know if this is useful
-    with st.sidebar:
-        st.markdown("Sidebar")
-        if st.session_state["logged_in"] == True:
-            user = st.session_state["user"]
-            st.markdown(f"Logged User Email: {user['email']}")
-
-
-
-           
-
-if st.session_state["role"] == "Attendee":
-    if st.session_state["page"] == "home":
-        st.markdown("welcome! View Upcoming Events!")
-        if st.button("Event Portal", key="view_events_btn", type="primary", use_container_width=True):
-            st.session_state["page"] = "Attendee"
+        if st.button("Create Event"):
+            events.append({
+                "id": str(uuid.uuid4()),
+                "name": name_input,
+                "date": date_input,
+                "time": time_input,
+                "location": location_input,
+                "description": description_input,
+                "tickets": tickets_input,
+                "reserved": 0
+            })
+            with open(events_file, "w") as f:
+               json.dump(events, f, indent=4)
+               with open(users_file, "r") as f:
+                    users = json.load(f)
+            st.success("Event created")
             st.rerun()
+            
 
- 
-else:
-    st.title("See Events")
 
-    event_names = []
 
-    for event in events:
-        event_names.append(event["name"])
 
-    selected_event_name = st.selectbox("Select Event", event_names)
 
-    selected_event = None
-    for event in events:
-        if event["name"] == selected_event_name:
-            selected_event = event
+    with tab3:
+        st.subheader("View and Update Event")
 
-    if selected_event:
-        st.markdown(f"Event: {selected_event["name"]}")
-        st.write(f" Date/Time: {selected_event["date"]} at {selected_event["time"]}")
-        st.write(f"Location:{selected_event["location"]}")
-        st.write(f"{selected_event["description"]}")
-        st.write(f"Tickets Left: {selected_event["tickets"] - selected_event["reserved"]}")
+        event_names = [event["name"] for event in events]
+        selected_name = st.selectbox("Select Event", event_names)
 
-        if st.button("Reserve Ticket"):
-            if selected_event["reserved"] < selected_event["tickets"]:
-                selected_event["reserved"] += 1
-                st.success("Ticket reserved!")
-            else:
-                st.error("Sold out")
+        selected_event = None
+        for event in events:
+            if event["name"] == selected_name:
+                selected_event = event
 
-                
+        if selected_event:
+            new_name = st.text_input("Event Name", selected_event["name"])
+            new_date = st.text_input("Date", selected_event["date"])
+            new_time = st.text_input("Time", selected_event["time"])
+            new_location = st.text_input("Location", selected_event["location"])
+            new_description = st.text_area("Description", selected_event["description"])
+            new_tickets = st.number_input("Tickets", value=selected_event["tickets"], min_value=1)
+
+            if st.button("Save Changes"):
+
+                for event in events:
+                    if event["id"] == selected_event["id"]:
+                        event["name"] = new_name
+                        event["date"] = new_date
+                        event["time"] = new_time
+                        event["location"] = new_location
+                        event["description"] = new_description
+                        event["tickets"] = new_tickets
+                        break
+
+                with open(events_file, "w") as f:
+                    json.dump(events, f, indent=4)
+
+                st.success("Event updated!")
+                st.rerun()
