f558ad091757508e6de0e9f798b2628dd2d71627
Matthew <mmusacch@udel.edu>
2026-05-10 13:41:59 -0400

    began event manager

---
+++ final_app_matt.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import json
+from pathlib import Path
+from datetime import datetime
+import uuid
+import time
+
+st.set_page_config("Event Manager", layout="wide", initial_sidebar_state="expanded")
