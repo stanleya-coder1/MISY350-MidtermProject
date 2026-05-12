4959fb5fbd426391ca21af8a9d40d61fcda8a4bf
Anastasia <stanleya@udel.edu>
2026-05-12 16:50:11 -0400

    register user definition auth servies

--- refactored/final part 2/services/authentication_service.py
+++ refactored/final part 2/services/authentication_service.py
@@ -1,5 +1,4 @@
 import uuid
-
 from data.data_manager import (load_data, save_data, users_file)
 
 
@@ -10,3 +9,21 @@ def login(email, password):
             return user
     return None
 
+def register_user(email, full_name, password, role):
+    users = load_data(users_file)
+    # duplicate email check
+    for user in users:
+        if user["email"].lower() == email.lower():
+            return "duplicate"
+
+    new_user = {
+        "id": str(uuid.uuid4()),
+        "email": email,
+        "full_name": full_name,
+        "password": password,
+        "role": role
+    }
+    users.append(new_user)
+
+    save_data(users_file, users)
+    return "success"
