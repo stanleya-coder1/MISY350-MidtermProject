0797c8a91f3a0e46c33fb15490b53562dad79bd2
Matthew <mmusacch@udel.edu>
2026-05-12 19:43:10 -0400

    Attendee final app refactor

--- refactored/final part 2/final_app2.py
+++ refactored/final part 2/final_app2.py
@@ -8,7 +8,6 @@ from ui.sidebar_ui import show_sidebar
 st.set_page_config(page_title="Event Manager", layout="wide", initial_sidebar_state="expanded") 
 
 #session state
-
 if "logged_in" not in st.session_state:
     st.session_state["logged_in"] = False
 
@@ -32,3 +31,13 @@ if page == "login":
 
 elif page == "register":
     show_register_page()
+
+# attendee pages
+elif page == "attendee_home":
+    show_attendee_home()
+
+elif page == "browse_events":
+    show_browse_events()
+
+elif page == "my_tickets":
+    show_my_tickets()
