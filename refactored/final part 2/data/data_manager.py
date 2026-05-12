8fd75ca3f9857eedddd8174df6a404a9a8529a69
Anastasia <stanleya@udel.edu>
2026-05-12 15:38:58 -0400

    data manger loading/importing functions and creating files

---
+++ refactored/final part 2/data/data_manager.py
@@ -0,0 +1,7 @@
+import json
+from pathlib import Path
+
+base_path = Path(__file__).resolve().parent.parent
+
+users_file = base_path / "users copy.json"
+events_file = base_path / "events copy.json"
